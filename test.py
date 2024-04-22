c1e7943d2f5bb485b433f88d684b8e2dc7d923dd
Hansal Shah <hansalshah100@gmail.com>
2024-04-22 00:15:18 +0000

    Updated the test file to use the iteratve fft module

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from fft import fft, ifft, nearest_pow_2
+from iterative_fft import fft, nearest_pow_2
 import librosa
 import numpy as np
 
@@ -9,6 +9,6 @@ reconstructed_audio = original_audio
 # Applying FFT and IFFT consecutively to test for accuracy issues with compounding errors
 number_of_iters = 20
 for i in range(number_of_iters):
-    reconstructed_audio = ifft(fft(reconstructed_audio), 1)
+    reconstructed_audio = fft(fft(reconstructed_audio, 0), 1)
 
 assert(np.allclose(np.append(original_audio, [0]*(nearest_pow_2(len(original_audio))-len(original_audio))), np.array(reconstructed_audio), atol=1e-6))
